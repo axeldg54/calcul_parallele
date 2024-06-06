07518b5f34e2ef441131291e5920966211946c02
axeldg54 <dung1u@etu.univ-lorraine.fr>
2024-06-06 08:16:08 +0200

    com

--- src/distributeur/Distributeur.java
+++ src/distributeur/Distributeur.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 
 public class Distributeur implements ServiceDistributeur, Serializable{
 
+    /**
+     * Ajoute un petit chinois en plus pour faire le calcul
+     * Salaire de 1€ par jour
+     */
+     * @param proxy
+     * @throws RemoteException
+     */
     @Override
     public void ajouterTravailleur(ServiceCalcul proxy) throws RemoteException{
         proxys.add(proxy);
